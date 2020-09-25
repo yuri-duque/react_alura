2eecc4fbf2fe96ea26d8e9001f43792956b5c5bf
Yuri Duque <yuriDuque>
2020-09-24 21:50:46 -0300

    ceep - configurando tela principal

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Teste</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..." />
+
+        <button>Criar Nota</button>
+      </form>
+
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </section>
   );
 }
 
