c997be7b8fc8f1df276804837e64b82d1b9d540b
Yuri Duque <yurithielmann83@gmail.com>
2020-10-08 14:27:06 -0300

    encerrando projeto ceep

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,11 +4,25 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+
+  constructor() {
+    super();
+    this.notas = [];
+    this.state = {}
+  }
+
+  criarNota(titulo, texto) {
+    const nota = { titulo, texto };
+    this.notas.push(nota);
+
+    this.setState({ notas: this.notas })
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.notas} />
       </section>
     );
   }
